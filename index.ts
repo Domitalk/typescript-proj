1b7a52fd2f434b370244239c69babf6361983536
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-19 10:36:54 -0400

    setting types in function args

--- index.ts
+++ index.ts
@@ -5,19 +5,24 @@ import ITodo from './interface/ITodo'
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
 axios.get(url).then(response => {
-    // console.log(response.data)
+    console.log(response.data)
     const todo: ITodo = response.data 
     // const todo = response.data as ITodo
 
-    const ID = todo.id
+    const id = todo.id
     const title = todo.title
-    const finished = todo.completed 
+    const completed = todo.completed 
 
-    console.log(`
-        The Todo with ID: ${ID} 
-        Has a title of: ${title}
-        Is it finished? ${finished}
-    `)
+    logTodo(id, title, completed)
 
 
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+        The Todo with ID: ${id} 
+        Has a title of: ${title}
+        Is it completed? ${completed}
+    `)
+
+}
