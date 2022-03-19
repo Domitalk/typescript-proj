67bcc420d7ca7dcec2dc411b5223ba05e84da886
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-19 11:26:42 -0400

    typing object types

---
+++ README.md
@@ -0,0 +1,18 @@
+NOTES 
+
+The annotations will be different between primitive vs object types 
+primitive types ie: 
+    number 
+    boolean
+    void 
+    undefined
+    string
+    symbol 
+    null 
+
+object types ie: 
+    functions
+    arrays
+    classes 
+    objects 
+
--- index.ts
+++ index.ts
@@ -15,7 +15,6 @@ axios.get(url).then(response => {
 
     logTodo(id, title, completed)
 
-
 })
 
 const logTodo = (id: number, title: string, completed: boolean) => {
@@ -24,5 +23,34 @@ const logTodo = (id: number, title: string, completed: boolean) => {
         Has a title of: ${title}
         Is it completed? ${completed}
     `)
+}
 
+let apples: number = 5 
+let speed: string = 'fast'
+let hasName: boolean = true 
+
+// this weird yo
+let nothingMuch: null = null 
+let nothing: undefined = undefined 
+
+// built in objects 
+// Date is a built in class/object 
+let now: Date = new Date 
+
+// Array 
+let colors: string[] = ['white', 'yellow', 'red', 'blue']
+let myNumber: number[] = [1, 2, 3, 4, 5]
+let truths: boolean[] = [true, true, true, false]
+
+// Classes 
+class Car {
+
+}
+let car: Car = new Car
+
+// Object literal 
+// seperate with ; 
+let point: { x: number; y: number; } = {
+    x: 10, 
+    y: 20
 }
