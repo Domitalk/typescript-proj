188b5992f595cd19ebe39daebf7799e6328951ce
Dominic Chu <dominic.m.chu@gmail.com>
2022-03-20 16:33:22 -0400

    use cases of when to use type annotations

--- index.ts
+++ index.ts
@@ -60,3 +60,46 @@ let point: { x: number; y: number; } = {
 const logNumber: (i: number) => void = (i) => {
     console.log(i)
 }
+
+// when to annotate the type manually? WHY WE DO THIS?
+// rely on type inference as much as we can 
+// but there are specific situations when we want to annotate
+
+// 1. When a function returns the ANY type 
+// 
+
+const json = '{ "x": 10, "y": 20 }'
+const coordinates: { x: number; y: number } = JSON.parse(json)
+console.log(coordinates)
+// JSON.parse() returns ANY type because it really depends on what the input is 
+// ie: JSON.parse('false')  will return boolean 
+// ie: JSON.parse('4') will return number 
+// ie: JSON.parse('{'whateverkey': 'whatevervalue'}') will return Object
+
+// 2. When declaring variable and initializing later 
+// ie: 
+// let something; 
+// something = 3
+
+let words = ['red', 'green', 'blue']
+let foundWord: boolean;  
+// let foundWord; 
+for (let i = 0; i < words.length; i++) {
+    if (words[i] == 'green') {
+        foundWord = true 
+    }
+}
+
+// basically seems like ts doesn't like ANY 
+
+// 3. When type can't be easily inferred even when the declaration/initialization is on same line 
+// 
+// in this specific situation we're gonna use a | to indicate OR aka between two types
+// obvo this bad example but still 
+let numbers = [-10, -1, 12]
+let numberAboveZero: boolean | number = false 
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+        numberAboveZero = numbers[i]
+    }
+}
